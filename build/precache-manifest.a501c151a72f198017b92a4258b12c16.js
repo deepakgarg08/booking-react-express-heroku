8c10124709a93d5afcfd7128c138a2a7543c775a
Deepak <erdkgarg08@gmail.com>
2020-07-02 00:46:58 +0200

    deploying react with bootstrap on heroku

---
+++ build/precache-manifest.a501c151a72f198017b92a4258b12c16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cadcce0aaa2f34f2bff9555cb0d0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a5e71851df329ad66a",
+    "url": "/static/css/2.bd9084f7.chunk.css"
+  },
+  {
+    "revision": "3468dd4ed71d8abd417d",
+    "url": "/static/css/main.3b6379cc.chunk.css"
+  },
+  {
+    "revision": "d3a5e71851df329ad66a",
+    "url": "/static/js/2.5c2cc6d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5c2cc6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3468dd4ed71d8abd417d",
+    "url": "/static/js/main.999ca1fd.chunk.js"
+  },
+  {
+    "revision": "239afc18b38daffb22ad",
+    "url": "/static/js/runtime-main.42f417a9.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd2945c59d1004e6d6c619b568929f1a.js"
+  "/precache-manifest.a501c151a72f198017b92a4258b12c16.js"
 );
 
 self.addEventListener('message', (event) => {
